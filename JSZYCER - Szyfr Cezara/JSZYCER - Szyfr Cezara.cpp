31e4ffdbd0cb5ce493eb7ef0d13f5949846afab2
Artur Andrulewicz <37901946+LionArt@users.noreply.github.com>
2018-05-14 20:01:37 +0200

    Add files via upload

---
+++ JSZYCER - Szyfr Cezara/JSZYCER - Szyfr Cezara.cpp
@@ -0,0 +1,29 @@
+/* Andrulewicz Artur
+KC009 - Odwracanie wyrazow
+2018-05-14
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string x;
+    while(getline(cin,x))
+    {
+        for(int i=0;i<x.length();i++)
+        {
+            if(x[i]>=65 && x[i]<=90)
+            {
+                x[i]=x[i]+3;
+                if(x[i]>90)
+                {
+                    x[i]=65+(x[i]-90-1);
+                }
+            }
+        }
+        cout<<x<<endl;
+    }
+    return 0;
+}
