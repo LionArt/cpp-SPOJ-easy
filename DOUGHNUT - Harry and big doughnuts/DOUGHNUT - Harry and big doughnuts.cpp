80992d1a849a36e7927f9c64325dc20b2f1f85d7
Artur Andrulewicz <37901946+LionArt@users.noreply.github.com>
2018-05-14 19:21:55 +0200

    Add files via upload

--- DOUGHNUT - Harry and big doughnuts/DOUGHNUT - Harry and big doughnuts.cpp
+++ DOUGHNUT - Harry and big doughnuts/DOUGHNUT - Harry and big doughnuts.cpp
@@ -1,6 +1,6 @@
 /* Andrulewicz Artur
 DOUGHNUT - Harry and big doughnuts
-2017-10-25
+2018-05-14
 */
 
 #include <iostream>
@@ -9,6 +9,16 @@ using namespace std;
 
 int main()
 {
-
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        int c,k,w;
+        cin>>c>>k>>w;
+        if((c*w)>k)
+        cout<<"no"<<endl;
+        else
+        cout<<"yes"<<endl;
+    }
     return 0;
 }
