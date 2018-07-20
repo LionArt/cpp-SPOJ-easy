206868a08d84b7835d78b97e528415ef52808643
Artur Andrulewicz <37901946+LionArt@users.noreply.github.com>
2018-07-20 20:46:05 +0200

    Add files via upload

---
+++ AL_17_01 - Mundial 2014/AL_17_01 - Mundial 2014.cpp
@@ -0,0 +1,23 @@
+/* Andrulewicz Artur
+AL_17_01 - Mundial 2014
+2018-07-20
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string napis="Mundial ";
+	char temp;
+	temp='R'-' ';
+	napis+=temp;
+	temp='P'-' ';
+	napis+=temp;
+	temp='Q'-' ';
+	napis+=temp;
+	temp='T'-' ';
+	napis+=temp;
+	cout<<napis<<endl;
+}
