0d1b1cea131921fe3cc2ebe39e28468bd4721246
Artur Andrulewicz <37901946+LionArt@users.noreply.github.com>
2018-07-27 20:16:00 +0200

    Add files via upload

---
+++ BAJTST - Bajtockie Święto Trójkąta/BAJTST - Bajtockie Święto Trójkąta.cpp
@@ -0,0 +1,44 @@
+/* Andrulewicz Artur
+BAJTST - Bajtockie Œwiêto Trójk¹ta
+2018-07-27
+*/
+
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+//Wzór na pole dowolnego trojkata Herona na podstawie znajomosci dlugosci 3 bokow
+double getPole(int a,int b,int c)
+{
+    if((a<(b+c)) && (b<(a+c)) && (c<(a+b)))
+    {
+        double p=double(a+b+c)/2;
+        double pole=sqrt(double((p*(p-a)*(p-b)*(p-c))));
+        return pole;
+    }
+    else
+    return 0;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        int osoby;
+        double kreda,wynik=0;
+        cin>>osoby>>kreda;
+        for(int j=0;j<osoby;j++)
+        {
+            int a,b,c;
+            cin>>a>>b>>c;
+            wynik+=((getPole(a,b,c))/10)*kreda;
+        }
+        wynik=round(wynik);
+        cout<<setprecision(1000)<<wynik<<endl;
+    }
+    return 0;
+}
